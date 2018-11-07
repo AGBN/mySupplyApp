076d15cf928e3fb02e4c7bdcc7d44d00b15fcd81
Anders Gert Birkbak Nielsen <1061694@ucn.dk>
2018-11-07 10:48:32 +0100

    Initial Setup of Core and Droid projects

---
+++ mySupplyApp.Core/App.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.ViewModels;
+using mySupplyApp.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mySupplyApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<TempViewModel>();
+        }
+    }
+}
--- mySupplyApp.Core/ViewModels/TempViewModel.cs
+++ mySupplyApp.Core/ViewModels/TempViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MvvmCross.ViewModels;
 
-namespace mySupplyApp.Core
+namespace mySupplyApp.Core.ViewModels
 {
-    public class Class1
+    public class TempViewModel : MvxViewModel
     {
+
     }
 }
